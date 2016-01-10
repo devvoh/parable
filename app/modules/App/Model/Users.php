6d0ffcdb320c87d1faf46941128c3f32e9cdede4
Robin de Graaf <hello@devvoh.com>
2016-01-10 22:59:18 +0100

    FIX: updated copyright notice in doc blocks

--- app/modules/App/Model/Users.php
+++ app/modules/App/Model/Users.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * @package     Fluid
- * @subpackage  test model entity
- * @copyright   2015 Robin de Graaf, devvoh webdevelopment
+ * @subpackage  Users example model
+ * @copyright   2015-2016, Robin de Graaf, devvoh webdevelopment
  * @license     MIT
  * @author      Robin de Graaf (hello@devvoh.com)
  */
