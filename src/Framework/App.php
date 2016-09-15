a19168f90f2502450fbfb437b18a68c17295e56d
Robin de Graaf <hello@devvoh.com>
2016-09-15 19:38:39 +0200

    Bump version to 0.8.11.

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -41,7 +41,7 @@ class App {
     protected $database;
 
     /** @var string */
-    protected $version = '0.8.10';
+    protected $version = '0.8.11';
 
     /**
      * @param \Parable\Filesystem\Path      $path
