0eba6d0197ec84ad1c1dcf4328dd627c58450d2e
Robin de Graaf <hello@devvoh.com>
2017-05-27 11:36:07 +0000

    Update dependencies and add small bit of logic to public/index.php to recognize composer install hasn't been run yet.

--- structure/public/index.php
+++ structure/public/index.php
@@ -1,6 +1,11 @@
 <?php
 ini_set('display_errors', '1');
 
-$app = require_once(__DIR__ . '/../vendor/devvoh/parable/src/Framework/Bootstrap.php');
+$bootstrapDirectory = __DIR__ . '/../vendor/devvoh/parable/src/Framework/Bootstrap.php';
+if (!file_exists($bootstrapDirectory)) {
+    die("<b>ERROR</b>: You need to run <pre style='display:inline-block;background:#e6e6e6;padding:2px 5px'>composer install</pre> before Parable will work.");
+}
+
+$app = require_once($bootstrapDirectory);
 
 $app->run();
