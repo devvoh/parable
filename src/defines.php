0c6240991d78c3fa6bcb6c1a73c73485c23a738b
Robin de Graaf <hello@devvoh.com>
2017-12-07 21:02:31 +0100

    Add defines.php so DS and BASEDIR are back but without odd bootstrapping.

---
+++ src/defines.php
@@ -0,0 +1,8 @@
+<?php
+// Here we define some constants Parable has a use of
+if (!defined("BASEDIR")) {
+    define("BASEDIR",  realpath(__DIR__ . "/../../../.."));
+}
+if (!defined("DS")) {
+    define("DS", DIRECTORY_SEPARATOR);
+}
