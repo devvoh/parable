8b8d39550a7260704479cc3f77f95ac9be89213d
Robin de Graaf <hello@devvoh.com>
2017-12-08 16:42:32 +0100

    Some small fixes based on feedback from @dmvdbrugge

--- src/defines.php
+++ src/defines.php
@@ -1,8 +1,8 @@
 <?php
 // Here we define some constants Parable has a use of
-if (!defined("BASEDIR")) {
-    define("BASEDIR", realpath(__DIR__ . "/../../../.."));
-}
 if (!defined("DS")) {
     define("DS", DIRECTORY_SEPARATOR);
+}
+if (!defined("BASEDIR")) {
+    define("BASEDIR", realpath(__DIR__ . DS . ".." . DS . ".." . DS . ".." . DS . ".."));
 }
